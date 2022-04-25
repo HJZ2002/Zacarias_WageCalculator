9f5ae8fcd4dfbc8dd0f315eabeabb157b58c99fc
zaccarias <zaccariah23@gmail.com>
2022-04-25 19:29:02 +0800

    try

--- app/src/main/java/com/example/zacarias_wagecalculator/View/MainActivity.java
+++ app/src/main/java/com/example/zacarias_wagecalculator/View/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.zacarias_wagecalculator;
+package com.example.zacarias_wagecalculator.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.zacarias_wagecalculator.R;
 
 public class MainActivity extends AppCompatActivity {
 
